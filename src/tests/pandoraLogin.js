9860bad981778c4bedcae83fd17a67ae0431287f
Alyssa Watson <lyssareba@gmail.com>
2017-12-05 16:59:46 -0700

    Element ID's are now placed in an array

--- src/tests/pandoraLogin.js
+++ src/tests/pandoraLogin.js
@@ -3,14 +3,16 @@ var user = {
   sessionID: '',
   username: '',
   password: '',
-  thumbsup: [],
+  thumbsup: {},
 }
 var endOfPage = false;
 var y;
 var mediaListId=[];
 var thumbsUp;
+var elementNum;
 
 module.exports = {
+
   'Step 1 - Login to Pandora' : function(browser) {
     browser
     .url('http://www.pandora.com/account/sign-in')
@@ -42,34 +44,85 @@ module.exports = {
     .useXpath()
     .waitForElementVisible("//*/span[@data-qa='thumbs_up_link']")
     .click("//*/span[@data-qa='thumbs_up_link']")
-    .getText("//*/span[@data-qa='thumbs_up_link']/span[2]")
-    .pause(1000)
-    .waitForElementVisible("//*/div[@class='MediaListItem'][1]")
-
-    .elements('xpath', "//*/div[@class='MediaListItem']", function(songs) {
-      console.log(songs.value);
-      mediaListId.push(songs.value[i].ELEMENT);
-      console.log(mediaListId);
-
-      if(mediaListId.length<thumbsUp-1) {
-        for(var i in songs){
-          if(songs.value[i].ELEMENT === undefined){
-            endOfPage=true;
-            return false;
-          };
-          if(songs.value[i].ELEMENT != undefined){
-            mediaListId.push(songs.value[i].ELEMENT);
-            browser.getLocationInView("(//*/div[@class='MediaListItem'])["+i+"]", function(res){
-              y = res.value.y
-              browser.pause(2000)
-              if(y>600){
-                browser.execute('scrollTo(0,600)')
-              };
-            })
-          };
-        };
-      }
-    })
     .pause(2000)
+    .getText("//*/span[@class='ProfileNav__count']", function(res){
+      thumbsUp=res.value;
+    })
+    .pause(1000)
+    .waitForElementVisible("(//*/div[@class='MediaListItem'])[1]")
+    .element('xpath', "(//*/div[@class='MediaListItem'])[1]", function(songs) {
+      var regex = new RegExp(/(\.)\d+/, 'g');
+      var regex2 = new RegExp(/(\-)\d+/, 'g');
+      user.sessionID = songs.value.ELEMENT.match(regex);
+      var temp = songs.value.ELEMENT.match(regex2);
+      elementNum = temp[0].slice(1);
+      mediaListId=getSongId();
+      browser.pause(5000);
+      user.thumbsup=getSongData();
+    })
+
+    function getSongId() {
+      var id = [];
+      var end = parseInt(thumbsUp) + parseInt(elementNum) - 1;
+      var start = elementNum;
+      for(var i = start; i < end; i++){
+        id.push('0'+ user.sessionID + '-' + i);
+      }
+      console.log(id);
+      return id;
+    }
+    function getSongData(){
+      var inView;
+      var songData= [];
+      console.log(mediaListId[0]);
+      browser.elementIdValue(mediaListId[0], function(res){
+        console.log(res);
+      })
+      
+      //  for(var i = 0; i <mediaListId.length-1; i++){
+      //    if(browser.elementIdLocationInView(mediaListId[i])){
+      //      inView=true;
+      //    } else {
+      //      inView=false;
+      //    }
+
+      //    if(inView){
+      //      browser.elementIdValue(mediaListId[i], function(res){
+      //        console.log(res);
+      //      })
+      //     //  songData.push()
+      //    } else {
+      //      browser.execute('scrollBy(200, 0)');
+      //      i--;
+      //    }
+      //   }
+       
+
+      return;
+    }
+
+    //         browser.getLocationInView("(//*/div[@class='MediaListItem'])["+i+"]", function(res){
+    //           y = res.value.y
+    //           browser.pause(2000)
+    //           if(y>600){
+    //             browser.execute('scrollTo(0,600)')
+    //           };
+    //         })
+    //       };
+
+    //       if(songs.value[i].ELEMENT != undefined){
+    //         mediaListId.push(songs.value[i].ELEMENT);
+    //         browser.getLocationInView("(//*/div[@class='MediaListItem'])["+i+"]", function(res){
+    //           y = res.value.y
+    //           browser.pause(2000)
+    //           if(y>600){
+    //             browser.execute('scrollTo(0,600)')
+    //           };
+    //         })
+    //       };
+    //     };
+    //   }
+    // })
+    browser.pause(2000)
   }
 };
